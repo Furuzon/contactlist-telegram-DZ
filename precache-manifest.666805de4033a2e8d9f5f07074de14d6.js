0b2822e90205af974f14e7a4e15411f8b0dbe52c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-01 10:10:57 +0000

    Deploy Furuzon/contactlist-telegram-DZ to github.com/Furuzon/contactlist-telegram-DZ.git:gh-pages

---
+++ precache-manifest.666805de4033a2e8d9f5f07074de14d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334b06a085c1ada5e255eafd73e33b8a",
+    "url": "/contactlist-telegram-DZ/index.html"
+  },
+  {
+    "revision": "0bb6d7aeda31cd99f9f5",
+    "url": "/contactlist-telegram-DZ/static/css/main.f788fea8.chunk.css"
+  },
+  {
+    "revision": "1adb9275c23705042c91",
+    "url": "/contactlist-telegram-DZ/static/js/2.c550c527.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/contactlist-telegram-DZ/static/js/2.c550c527.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bb6d7aeda31cd99f9f5",
+    "url": "/contactlist-telegram-DZ/static/js/main.17f38fca.chunk.js"
+  },
+  {
+    "revision": "0737f8dbe7046e8196bf",
+    "url": "/contactlist-telegram-DZ/static/js/runtime-main.70470171.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contactlist-telegram-DZ.git/precache-manifest.df91e15edec61932e51aedf292f2f8f2.js"
+  "/contactlist-telegram-DZ/precache-manifest.666805de4033a2e8d9f5f07074de14d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contactlist-telegram-DZ.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contactlist-telegram-DZ/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
