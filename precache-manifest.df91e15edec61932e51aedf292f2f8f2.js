561254fe65372f2b0ea7805aa2ebde9fecfd5e3d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-01 09:50:15 +0000

    Deploy Furuzon/contactlist-telegram-DZ to github.com/Furuzon/contactlist-telegram-DZ.git:gh-pages

---
+++ precache-manifest.df91e15edec61932e51aedf292f2f8f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea00fba52bf433e5fca6a04b5fe9c78",
+    "url": "/contactlist-telegram-DZ.git/index.html"
+  },
+  {
+    "revision": "13e7fd92c5125a7b091e",
+    "url": "/contactlist-telegram-DZ.git/static/css/main.f788fea8.chunk.css"
+  },
+  {
+    "revision": "1adb9275c23705042c91",
+    "url": "/contactlist-telegram-DZ.git/static/js/2.c550c527.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/contactlist-telegram-DZ.git/static/js/2.c550c527.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13e7fd92c5125a7b091e",
+    "url": "/contactlist-telegram-DZ.git/static/js/main.65ec9b7a.chunk.js"
+  },
+  {
+    "revision": "7ecdcc610b8043b580b6",
+    "url": "/contactlist-telegram-DZ.git/static/js/runtime-main.64542313.js"
+  }
+]);
